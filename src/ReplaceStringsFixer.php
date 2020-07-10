5bf4a1e8b5549355dbe786fcd65bd3d4f2b3440c
Johan Björk <johanimon@gmail.com>
2020-07-10 03:42:22 +0200

    Implement ReplaceStringsFixer

---
+++ src/ReplaceStringsFixer.php
@@ -0,0 +1,108 @@
+<?php
+declare(strict_types=1);
+
+namespace YSDS\Lint;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\Fixer\ConfigurationDefinitionFixerInterface;
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolver;
+use PhpCsFixer\FixerConfiguration\FixerOption;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+
+class ReplaceStringsFixer extends AbstractFixer implements ConfigurationDefinitionFixerInterface
+{
+    const OPTION_REPLACEMENTS = 'replacements';
+    const OPTION_FIX_COMMON = 'fix_common';
+
+    const COMMON_FIXES = [
+        // no break space
+        // https://www.fileformat.info/info/unicode/char/00a0/index.htm
+        "\u{00A0}" => "\u{0020}",
+
+        // zero width space
+        // https://www.fileformat.info/info/unicode/char/200b/index.htm
+        "\u{200B}" => '',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected array $replacements = [];
+
+    public function configure(array $configuration = null)
+    {
+        parent::configure($configuration);
+
+        $replacements = $this->configuration[static::OPTION_REPLACEMENTS] ?? [];
+
+        if ($this->configuration[static::OPTION_FIX_COMMON] ?? false) {
+            $replacements = array_merge($replacements, static::COMMON_FIXES);
+        }
+
+        $this->replacements = $replacements;
+    }
+
+    protected function applyFix(\SplFileInfo $file, Tokens $tokens)
+    {
+        for ($i = 0; $i < $tokens->count(); ++$i) {
+            /* @var Token $token */
+            $token = $tokens[$i];
+
+            $original = $token->getContent();
+            $fixed = strtr($original, $this->replacements);
+
+            if ($original !== $fixed) {
+                $kind = $token->getId();
+                $tokens->overrideRange($i, $i, [
+                    $token->isArray()
+                        ? new Token([$kind, $fixed])
+                        : new Token($fixed),
+                ]);
+            }
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDefinition()
+    {
+        return new FixerDefinition('Replaces matched strings.', [
+            new CodeSample("<?php echo \'Yee!\';", [
+                static::OPTION_REPLACEMENTS => [
+                    'Yee' => 'Boi',
+                ],
+            ]),
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isCandidate(Tokens $tokens)
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getConfigurationDefinition()
+    {
+        return new FixerConfigurationResolver([
+            new FixerOption(static::OPTION_FIX_COMMON, 'Apply common fixes (no break space, zero width space).', false),
+            new FixerOption(static::OPTION_REPLACEMENTS, 'Array of key-value replacements.', false),
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return 'YSDS/' . parent::getName();
+    }
+}
---
+++ tests/ReplaceStringsFixerTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace YSDS\Lint\Tests;
+
+use PhpCsFixer\Test\AbstractFixerTestCase;
+use YSDS\Lint\ReplaceStringsFixer;
+
+class ReplaceStringsFixerTest extends AbstractFixerTestCase
+{
+    public function createFixer()
+    {
+        return new ReplaceStringsFixer();
+    }
+
+    public function replaceProvider()
+    {
+        return [
+            [['yee' => 'boi'], "<?php return 'boi!';", "<?php return 'yee!';"],
+            [['a' => 'c', 'b' => 'd'], "<?php return 'cd';", "<?php return 'ab';"],
+            [['a' => 'b'], "<?php return 'cc';"],
+            [['red' => 'blue'], "<?php return 'blue blue';", "<?php return 'red red';"],
+        ];
+    }
+
+    /**
+     * @dataProvider replaceProvider
+     * @param array $replacements
+     * @param string $expected
+     * @param string|null $input
+     */
+    public function testReplace(array $replacements, string $expected, ?string $input = null)
+    {
+        $this->fixer->configure([
+            ReplaceStringsFixer::OPTION_REPLACEMENTS => $replacements,
+        ]);
+
+        $this->doTest($expected, $input);
+    }
+
+    public function testCommonFixes()
+    {
+        $this->fixer->configure([
+            ReplaceStringsFixer::OPTION_FIX_COMMON => true,
+        ]);
+
+        $this->doTest("<?php return '\u{0020}';", "<?php return '\u{00A0}';");
+    }
+}
